67af08132c2d41398213da5839cb64fb5786e8c8
AhmedHajiyev <hajiyev.ahmed01@gmail.com>
2022-02-04 23:39:11 +0400

    uptaded

---
+++ SimpsonBook/SimpsonBook/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Ahmed Hajıyev on 04.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var mySimpsons = [Simpson]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+        
+        
+        //Simpson Objects
+        let homer = Simpson(simpsonName: "Homer Simpson", simpsonJob: "Nuclear Safety", simpsonImage: UIImage(named: "homerSimpson")!)
+        let marge = Simpson(simpsonName: "Marge Simpson", simpsonJob: "Housewife", simpsonImage: UIImage(named: "margeSimpson")!)
+        let bart = Simpson(simpsonName: "Bart Simpson", simpsonJob: "Student", simpsonImage: UIImage(named: "bartSimpsons")!)
+        let bruns = Simpson(simpsonName: "Mr.Bruns", simpsonJob: "Workless", simpsonImage: UIImage(named: "mrBruns")!)
+        let ned = Simpson(simpsonName: "Ned Fleanders", simpsonJob: "Mechanic", simpsonImage: UIImage(named: "nedFlanders")!)
+        
+        
+        
+        mySimpsons.append(homer)
+        mySimpsons.append(marge)
+        mySimpsons.append(bart)
+        mySimpsons.append(bruns)
+        mySimpsons.append(ned)
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mySimpsons.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = mySimpsons[indexPath.row].name
+        return cell
+    }
+    
+}
+
---
+++ SimpsonBook/SimpsonBook/detailsVC.swift
@@ -0,0 +1,26 @@
+//
+//  detailsVC.swift
+//  SimpsonBook
+//
+//  Created by Ahmed Hajıyev on 04.02.22.
+//
+
+import UIKit
+
+class detailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var jobLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
